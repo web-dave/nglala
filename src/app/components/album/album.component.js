d00518eef37a3b9c7bd30d3881857bf05b1eef48
David Müllerchen <damu1de@resco.local>
2016-07-19 10:41:13 +0200

    Album componentRouter

--- src/app/components/album/album.component.js
+++ src/app/components/album/album.component.js
@@ -2,20 +2,21 @@
     'use strict';
 
     /* @ngInject */
-    function albumController($routeParams, spotifyService, $sce) {
+    function albumController(spotifyService, $sce) {
         var vm = this;
         vm.album = '';
 
-        function constructor() {
-
-            spotifyService.getAlbum($routeParams.id).then(function(response){
+        this.$routerOnActivate = function (next) {
+            spotifyService.getAlbum(next.params.id).then(function(response){
                 vm.album = response.data;
             });
-        }
+        };
+
+        function constructor() {}
 
         vm.getAudioUrl = function(url) {
             return $sce.trustAsResourceUrl(url);
-        }
+        };
 
         constructor();
     }
