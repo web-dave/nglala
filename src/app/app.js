d64cafec4a388306a8351dabe7ab023fb25231a8
David Müllerchen <damu1de@resco.local>
2016-07-19 10:02:44 +0200

    set ComponentRouter as dependency

--- src/app/app.js
+++ src/app/app.js
@@ -1,22 +1,26 @@
 (function () {
     'use strict';
-    angular.module('nglala', ['ngRoute'])
-        .config(function ($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    template: '<search><search>'
-                })
-                .when('/artist/:id', {
-                    template: '<artist><artist>'
-                })
-                .when('/album/:id', {
-                    template: '<album></album>'
-                })
-                .when('/about', {
-                    template: '<about><about>'
-                })
-                .otherwise({ redirectTo: '/' });
-        });
+    angular.module('nglala', ['ngComponentRouter'])
+        .config(function ($locationProvider) {
+            $locationProvider.html5Mode(true);
+        })
+        .value('$routerRootComponent', 'search')
+        // .config(function ($routeProvider) {
+        //     $routeProvider
+        //         .when('/', {
+        //             template: '<search><search>'
+        //         })
+        //         .when('/artist/:id', {
+        //             template: '<artist><artist>'
+        //         })
+        //         .when('/album/:id', {
+        //             template: '<album></album>'
+        //         })
+        //         .when('/about', {
+        //             template: '<about><about>'
+        //         })
+        //         .otherwise({ redirectTo: '/' });
+        // });
 
     angular.element(document).ready(function () {
         angular.bootstrap(document.body, ['nglala']);
