35a17e631e8f5e95a037a4400b89de0a152e261a
David Müllerchen <damu1de@resco.local>
2016-07-19 09:49:46 +0200

    helper alle used technics in one file

---
+++ src/app/shared/helper.js
@@ -0,0 +1,82 @@
+// module
+(function () {
+    'use strict';
+
+    angular.module('modulename', []);
+})();
+// directive
+(function () {
+    'use strict';
+
+    angular
+        .module('modulename')
+        .directive('myDir', myDir);
+    /* @ngInject */
+    function myDir(dependency1) {
+        var directive = {
+            bindToController: true,
+            controller: myController,
+            controllerAs: '$ctrl',
+            link: link,
+            restrict: 'A',
+            scope: {
+            }
+        };
+        return directive;
+
+        function link(scope, element, attrs) { }
+    }
+    /* @ngInject */
+    function myController() { }
+})();
+// component
+(function () {
+    'use strict';
+
+    angular
+        .module('modulename')
+        .component('myComp', myComp);
+
+    /* @ngInject */
+    function myCompController() {
+        var vm = this;
+
+        function constructor() { }
+
+        constructor();
+    }
+
+    var myComp = {
+        templateUrl: 'app/components/myComp/myComp.html',
+        controller: myCompController
+    };
+})();
+// service
+(function () {
+    'use strict';
+
+    angular
+        .module('modulename')
+        .service('myserv', myserv);
+
+    /* @ngInject */
+    function myserv($http) {
+        var vm = this;
+        vm.serviceMethode = function () { }
+    }
+})();
+// filter
+(function () {
+    'use strict';
+
+    angular
+        .module('modulename')
+        .filter('myfilter', myfilter);
+
+    function myfilter() {
+        return function (input) {
+            var output = input;
+            return output;
+        };
+    }
+})();
