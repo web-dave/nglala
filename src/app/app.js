b3992a819b9195b2146aca553ac51979e97a3e3d
David Müllerchen <damu1de@resco.local>
2016-07-21 14:33:57 +0200

    html5Mode

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,10 @@
 (function () {
     'use strict';
     angular.module('nglala', ['ngComponentRouter'])
-        .value('$routerRootComponent', 'spotify');
+        .value('$routerRootComponent', 'spotify')
+        .config(function ($locationProvider) {
+            $locationProvider.html5Mode(true);
+        })
 
     angular.element(document).ready(function () {
         angular.bootstrap(document.body, ['nglala']);
